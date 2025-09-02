5dc85504633e444dcfad2043ced005642c86bd13
BuckSizzler <andybray4@gmail.com>
2025-09-02 14:16:01 +0100

    changes 0209

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astonishing-haupia-82dc93.netlify.app/",
+  integrations: [preact()]
+});
